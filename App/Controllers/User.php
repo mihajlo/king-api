3f2e021fe4a359a5790947e3332492c6e981b5b6
Mihajlo Siljanoski <mihajlo.siljanoski@gmail.com>
2017-11-17 15:32:58 +0100

    * The MIT License  *  * Copyright 2017 mihajlo.  *  * Permission is hereby granted, free of charge, to any person obtaining a copy  * of this software and associated documentation files (the "Software"), to deal  * in the Software without restriction, including without limitation the rights  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  * copies of the Software, and to permit persons to whom the Software is  * furnished to do so, subject to the following conditions:  *  * The above copyright notice and this permission notice shall be included in  * all copies or substantial portions of the Software.  *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN  * THE SOFTWARE.

--- App/Controllers/User.php
+++ App/Controllers/User.php
@@ -1,16 +1,43 @@
 <?php
 
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2017 mihajlo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 /**
- * Description of User
+ * @project KING API - Framework for Rest APIs 
+ * @project_url https://king.php.mk/
+ * 
  *
- * @author mihajlo
+ * @author Mihajlo Siljanoski
+ * @url https://mk.linkedin.com/in/msiljanoski
+ * @email mihajlo.siljanoski@gmail.com
+ * @skype mihajlo.siljanoski
+ * 
  */
+
+
 class User {
     public static function index($db,$a,$b){
         
@@ -24,8 +51,8 @@ class User {
         $user=Auth::validate();
         
         $mail=Lib::load('Email',Config::item('email_config'));
-        error_reporting(E_ALL);
-        ini_set('display_errors',1);
+        
+        //more info: http://xmodule.eco.mk/module/validation
         $validator=Lib::load('validator');
         
         $rules_array = array(
--- App/Libraries/filedb.php
+++ App/Libraries/filedb.php
@@ -1,6 +1,33 @@
 <?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 mihajlo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 /**
- * Non-sql database based on file system. 
+ * @project KING API - Framework for Rest APIs 
+ * @project_url https://king.php.mk/
+ * 
  *
  * @author Mihajlo Siljanoski
  * @url https://mk.linkedin.com/in/msiljanoski
@@ -8,6 +35,8 @@
  * @skype mihajlo.siljanoski
  * 
  */
+
+
 class filedb {
     //put your code here
     public function __construct($database,$path='databases') {
